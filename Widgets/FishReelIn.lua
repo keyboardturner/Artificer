c92658f8e09ed9bd9deedcb79c99fbdcc0a2b3c2
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2026-09-17 23:47:27 -0700

    fix fish

--- Widgets/FishReelIn.lua
+++ Widgets/FishReelIn.lua
@@ -12,11 +12,25 @@ for k, v in pairs(fishreelin.EventsList) do
 	fishreelin:RegisterEvent(v);
 end
 
+local spellIDs = {
+	[131476] = true, -- mainline
+
+	[7620] = true, -- apprentice fishing
+	[7731] = true, -- 50+
+	[7732] = true, -- 125+
+	[18248] = true, -- 200+
+	[33095] = true, -- 275+
+	[51294] = true, -- 350+
+	[88868] = true, -- 425+
+	[110410] = true, -- 500+
+	[158743] = true, --575+
+};
+
 function fishreelin.event(self, event, ...)
 	if Artificer_DB.Widgets.FishReelIn then
 		if event == "UNIT_SPELLCAST_CHANNEL_STOP" then
-			local unitTarget, castGUID, spellID = ...
-			if not issecretvalue(unitTarget) and unitTarget == "player" and not issecretvalue(spellID) and spellID == 131476 then
+			local unitTarget, castGUID, spellID = ...;
+			if not issecretvalue(unitTarget) and unitTarget == "player" and not issecretvalue(spellID) and spellIDs[spellID] then
 				PlaySoundFile(569808);
 			end
 		end
